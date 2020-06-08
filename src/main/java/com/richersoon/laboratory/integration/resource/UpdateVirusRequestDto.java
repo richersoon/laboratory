10e2f65a3c2fdb038f1e2593b963d65299a5012b
Ramon <richersoon@gmail.com>
2020-06-08 17:30:07 +0800

    Added get update viruse endpoint in virus resource. Fixed class level url in virus resource.

---
+++ src/main/java/com/richersoon/laboratory/integration/resource/UpdateVirusRequestDto.java
@@ -0,0 +1,8 @@
+package com.richersoon.laboratory.integration.resource;
+
+import lombok.Data;
+
+@Data
+public class UpdateVirusRequestDto {
+    private String description;
+}
--- src/main/java/com/richersoon/laboratory/integration/resource/VirusResource.java
+++ src/main/java/com/richersoon/laboratory/integration/resource/VirusResource.java
@@ -8,14 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Endpoint that expose viruses functionalities
  */
-@RestController("viruses")
+@RestController
+@RequestMapping("/viruses")
 public class VirusResource {
 
     @Autowired
@@ -33,6 +37,16 @@ public class VirusResource {
                 .description(createRequestDto.getDescription())
                 .build();
         return new ResponseEntity<>(virusService.create(requestDto), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{name}")
+    public VirusDto update(@RequestBody UpdateVirusRequestDto updateVirusRequestDto,
+                           @PathVariable String name) {
+        VirusRequestDto requestDto = VirusRequestDto.builder()
+                .name(name)
+                .description(updateVirusRequestDto.getDescription())
+                .build();
+        return virusService.update(requestDto);
     }
 
     /**
