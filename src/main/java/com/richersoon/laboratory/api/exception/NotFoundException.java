e9fe754cbba2973e880b0101eb53305cfc86f633
Ramon <richersoon@gmail.com>
2020-06-08 17:37:06 +0800

    Added http exception in laboratory exception implementations.

--- src/main/java/com/richersoon/laboratory/api/exception/NotFoundException.java
+++ src/main/java/com/richersoon/laboratory/api/exception/NotFoundException.java
@@ -1,8 +1,12 @@
 package com.richersoon.laboratory.api.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Exception whenever to prevent redundancy by value
  */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NotFoundException extends LaboratoryException {
     public static final String MESSAGE = "Not found";
 
